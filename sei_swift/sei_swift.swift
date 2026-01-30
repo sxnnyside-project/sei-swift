08f05fe7f6ea5edca735b52c33ae4e364ac21867
Houjou Sxnnyside <devildollzetaalpha@gmail.com>
2026-01-30 09:20:47 -0600

    Initial Commit

---
+++ sei_swift/sei_swift.swift
@@ -0,0 +1,9 @@
+//
+//  sei_swift.swift
+//  sei_swift
+//
+//  Created by TI on 30/01/26.
+//
+
+import Foundation
+
---
+++ sei_swiftTests/sei_swiftTests.swift
@@ -0,0 +1,17 @@
+//
+//  sei_swiftTests.swift
+//  sei_swiftTests
+//
+//  Created by TI on 30/01/26.
+//
+
+import Testing
+@testable import sei_swift
+
+struct sei_swiftTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
